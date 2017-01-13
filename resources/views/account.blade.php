c7ff62c2d54d94d4c7dffd80d52812498edae6d4
EmperorTrajan <rojeantingal@gmail.com>
2017-01-13 15:35:59 +0800

    Added Lastname, phone and password fields in account.blade.php

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -7,11 +7,23 @@
 @section('content')
     <section class="row new-post">
         <div class="col-md-6 col-md-offset-3">
-            <header><h3>Your Account</h3></header>
+            <header><h3>User Profile </h3></header>
             <form action="{{ route('account.save') }}" method="post" enctype="multipart/form-data">
                 <div class="form-group">
                     <label for="first_name">First Name</label>
                     <input type="text" name="first_name" class="form-control" value="{{ $user->first_name }}" id="first_name">
+                </div>
+                <div class="form-group">
+                    <label for="last_name">Last Name</label>
+                    <input type="text" name="last_name" class="form-control" value="{{ $user->last_name }}" id="last_name">
+                </div>
+                <div class="form-group">
+                    <label for="Phone">Phone Number</label>
+                    <input type="text" name="phone" class="form-control" value="{{ $user->phone }}" id="phone">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input class="form-control" type="text" name="password" id="password" value="{{ $user->password }}" id="password"> <!---->
                 </div>
                 <div class="form-group">
                     <label for="image">Image (only .jpg)</label>
